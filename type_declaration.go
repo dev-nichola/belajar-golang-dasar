f3ef8ebd6ee51021fb6d24de1ce4cf24c98822ad
dev-nichola <kerjasamakol@gmail.com>
2024-03-11 19:51:17 +0700

    type declaration

--- type_declaration.go
+++ type_declaration.go
@@ -6,6 +6,13 @@ func main() {
 	type NoKTP string
 
 	var ktp NoKTP = "111111111111"
+
 	fmt.Println(ktp)
-	fmt.Println()
+
+	// Konversi Ke Tipe Data NoKTP
+	var contoh string = "222222222"
+	fmt.Println(NoKTP(contoh))
+
+	var konversi NoKTP = NoKTP(contoh)
+	fmt.Println(konversi)
 }
